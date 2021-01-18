2232e7d546a1a25b82514989abf4c80149c56e5c
Fridah-Alwanga <fridahalwanga6@gmail.com>
2021-01-18 12:47:15 +0300

    update models

--- socialmedia/admin.py
+++ socialmedia/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile,Post
+from .models import Post
 
-admin.site.register(Profile)
+# Register your models here.
 admin.site.register(Post)
--- socialmedia/models.py
+++ socialmedia/models.py
@@ -1,60 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
-from tinymce.models import HTMLField
-
-
+from django.utils import timezone
+from django.contrib.auth.models import User 
+from django.urls import reverse
 
 # Create your models here.
-class Profile(models.Model):
-    name = models.CharField(max_length=30)
-    profile_pic = models.ImageField(upload_to='new_post/', blank = 'true')
-    bio = models.TextField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default='', null=True)
-
-    def save_profile(self):
-        self.save
-
-    def delete_user(self):
-        self.delete()
-
-    
-    @classmethod
-    def update_profile(cls, id, value):
-        cls.objects.filter(id=id).update(profile_name=value)
-
-   
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
-
 class Post(models.Model):
-    image = models.ImageField(upload_to='new_post/', blank=True ,default = 'default.jpg')
-    title = models.CharField(max_length=30, default='')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default='', null=True ,related_name='author')
-    caption = models.TextField(max_length=300)
-    # likes = models.IntegerField(User, default=None, blank =True)
-
-    @classmethod
-    def all_posts(cls) :
-        posts = cls.objects.all()
-        return posts
-
-    def save_post(self):
-        self.save()
-
-    def delete_post(self):
-        self.delete()
-
-    @classmethod
-    def update_post(cls, id, value):
-        cls.objects.filter(id=id).update(image=value)
-
-
-
+    image = models.ImageField(upload_to='posts/')
+    title = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-    @property
-    def num_likes(self):
-        return self.liked.all.count()
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
